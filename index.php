9a7c98e6db9b9f00f47b0d114216133c44c539bb
Levi Bostian <levi.bostian@gmail.com>
2015-03-26 14:56:51 -0500

    App icon and name appear on top of screen with roboto font.

---
+++ index.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Document</title>
+
+    <!-- Bootstrap -->
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+    <link href="http://fonts.googleapis.com/css?family=Roboto:300" rel="stylesheet" type="text/css">
+
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-12">
+          <img alt="app logo" src="img/app-logo.png" height="75px" width="75px"/>
+          <h1 class="roboto inline-text" style="padding-left: 10px;">App Name</h1>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
