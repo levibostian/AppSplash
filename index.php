6ab6a57e93840724788a499c3240334d8d341cdd
Levi Bostian <levi.bostian@gmail.com>
2015-03-28 16:17:59 -0500

    Add header for a nice looking background. Position the phone screen and text.

--- index.php
+++ index.php
@@ -10,15 +10,44 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
     <link href="http://fonts.googleapis.com/css?family=Roboto:300" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 
     <link rel="stylesheet" href="style.css">
   </head>
   <body>
-    <div class="container">
-      <div class="row">
-        <div class="col-xs-12">
-          <img alt="app logo" src="img/app-logo.png" height="75px" width="75px"/>
-          <h1 class="roboto inline-text" style="padding-left: 10px;">App Name</h1>
+    <div class="container-fluid app-header">
+      <div class="container">
+        <div class="row">
+          <div>
+            <img alt="app logo" src="img/app-logo.png" height="75px" width="75px"/>
+            <h1 class="inline-text" style="padding-left: 10px;">App Name</h1>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-md-offset-1 col-md-6 center-vertical">
+            <h2 class="description-header">App for people to do stuff.</h2>
+            <p class="description">Dr. Jimmy Brungus forgot to do rockets. Hippie Joel tastes like cow bathroom. Not for horses. No lonely times just dreams you turkey. Stop, drop and roll bones made from stuff your muscles don't like. Check the expiration date used to love pruppets. </p>
+          </div>
+          <div class="col-md-4 app-screenshot-container">
+            <img alt="app screenshot" src="img/app-screenshot.png" height="700px"/>
+          </div>
+        </div>
+      </div>
+
+      <div class="row" style="background:white;">
+        <div class="col-md-offset-9 col-md-3">
+          <a href="#" class="white">
+            <div class="social-button" style="background:#3b5998;">
+              <i class="fa fa-facebook fa-4x icon"></i>
+            </div>
+          </a>
+          
+          <a href="#" class="white">
+            <div class="social-button" href="#" style="background:#4099FF">
+              <i class="fa fa-twitter fa-4x icon"></i>
+            </div>
+          </a>
         </div>
       </div>
     </div>
