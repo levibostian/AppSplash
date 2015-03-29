44f4d8b3ff54b3e9637279b080d94baa444da3c0
Levi Bostian <levi.bostian@gmail.com>
2015-03-29 12:59:07 -0500

    Adding centering horizontal and vertical div classes trying to center elements on page. White row of site broken.

--- index.php
+++ index.php
@@ -37,56 +37,60 @@
                         <img alt="app screenshot" src="img/app-screenshot.png" height="625px"/>
                     </div>
                 </div><!-- row -->
-            </div><!-- container -->
+
 
             <div class="row connect-row">
                 <div class="container">
-                    <div class="col-md-6">
+                    <div class="row">
+                        <div class="col-md-6">
 
-                        <!-- Begin MailChimp Signup Form -->
-                        <div id="mc_embed_signup">
-                            <form action="//levibostian.us10.list-manage.com/subscribe/post?u=7f489ab74730d111936a8515e&amp;id=e7c869cc60" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-                                <div id="mc_embed_signup_scroll">
-	                            
-                                    <div class="mc-field-group mc_field_headers">
-	                                <input type="email" placeholder="Email Address" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+                            <!-- Begin MailChimp Signup Form -->
+                            <div id="mc_embed_signup">
+                                <form action="//levibostian.us10.list-manage.com/subscribe/post?u=7f489ab74730d111936a8515e&amp;id=e7c869cc60" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+                                    <div id="mc_embed_signup_scroll">
+	                                
+                                        <div class="mc-field-group mc_field_headers">
+	                                    <input type="email" placeholder="Email Address" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+                                        </div>
+	                                <div id="mce-responses" class="clear">
+		                            <div class="response" id="mce-error-response" style="display:none"></div>
+		                            <div class="response" id="mce-success-response" style="display:none"></div>
+	                                </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                                        <div style="position: absolute; left: -5000px;"><input type="text" name="b_7f489ab74730d111936a8515e_e7c869cc60" tabindex="-1" value=""></div>
+                                        <div class="clear"><input type="submit" value="Notify" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
                                     </div>
-	                            <div id="mce-responses" class="clear">
-		                        <div class="response" id="mce-error-response" style="display:none"></div>
-		                        <div class="response" id="mce-success-response" style="display:none"></div>
-	                            </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-                                    <div style="position: absolute; left: -5000px;"><input type="text" name="b_7f489ab74730d111936a8515e_e7c869cc60" tabindex="-1" value=""></div>
-                                    <div class="clear"><input type="submit" value="Notify" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
-                                </div>
-                            </form>
-                        </div>
-                        <script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
-                        <!--End mc_embed_signup-->
-                        
-                    </div>
-                    <div class="col-md-6">
-                        <div class="center-block">
-                            <a href="#" class="white">
-                                <div class="social-button facebook">
-                                    <i class="fa fa-facebook fa-4x icon"></i>
-                                </div>
-                            </a>
+                                </form>
+                            </div>
+                            <script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
+                            <!--End mc_embed_signup-->
                             
-                            <a href="#" class="white">
-                                <div class="social-button twitter" href="#">
-                                    <i class="fa fa-twitter fa-4x icon"></i>
-                                </div>
-                            </a>
+                        </div><!-- col-md-6 -->
+                        
+                        <div class="col-md-6">
+                            <div class="center-hor-vert">
+                                <a href="#" class="white">
+                                    <div class="social-button facebook">
+                                        <i class="fa fa-facebook fa-4x icon"></i>
+                                    </div>
+                                </a>
+                                
+                                <a href="#" class="white">
+                                    <div class="social-button twitter" href="#">
+                                        <i class="fa fa-twitter fa-4x icon"></i>
+                                    </div>
+                                </a>
 
-                            <a href="#" class="white">
-                                <div class="social-button instagram" href="#">
-                                    <i class="fa fa-instagram fa-4x icon"></i>
-                                </div>
-                            </a>
-                        </div><!-- center-block -->
-                    </div><!-- col-md-offset-6 col-md-6 -->
+                                <a href="#" class="white">
+                                    <div class="social-button instagram" href="#">
+                                        <i class="fa fa-instagram fa-4x icon"></i>
+                                    </div>
+                                </a>
+                            </div><!-- center-hor-vert -->
+                        </div><!-- col-md-offset-6 col-md-6 -->
+                    </div><!-- row -->
                 </div><!-- container -->
             </div><!-- row connect-row -->
+                        </div><!-- container -->
         </div><!-- container-fluid -->
     </body>
 </html>
