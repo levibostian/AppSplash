4ed3be846a24dcef90bed6aceab0d679f96a5829
Levi Bostian <levi.bostian@gmail.com>
2015-03-28 20:34:59 -0500

    Add mailchimp notify form.

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
         <link href="http://fonts.googleapis.com/css?family=Roboto:300" rel="stylesheet" type="text/css">
         <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+        <link href="//cdn-images.mailchimp.com/embedcode/classic-081711.css" rel="stylesheet" type="text/css">
 
         <link rel="stylesheet" href="style.css">
     </head>
@@ -40,7 +41,30 @@
 
             <div class="row connect-row">
                 <div class="container">
-                    <div class="col-md-offset-6 col-md-6">
+                    <div class="col-md-6">
+
+                        <!-- Begin MailChimp Signup Form -->
+                        <div id="mc_embed_signup">
+                            <form action="//levibostian.us10.list-manage.com/subscribe/post?u=7f489ab74730d111936a8515e&amp;id=e7c869cc60" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+                                <div id="mc_embed_signup_scroll">
+	                            
+                                    <div class="mc-field-group mc_field_headers">
+	                                <input type="email" placeholder="Email Address" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+                                    </div>
+	                            <div id="mce-responses" class="clear">
+		                        <div class="response" id="mce-error-response" style="display:none"></div>
+		                        <div class="response" id="mce-success-response" style="display:none"></div>
+	                            </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                                    <div style="position: absolute; left: -5000px;"><input type="text" name="b_7f489ab74730d111936a8515e_e7c869cc60" tabindex="-1" value=""></div>
+                                    <div class="clear"><input type="submit" value="Notify" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+                                </div>
+                            </form>
+                        </div>
+                        <script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
+                        <!--End mc_embed_signup-->
+                        
+                    </div>
+                    <div class="col-md-6">
                         <div class="center-block">
                             <a href="#" class="white">
                                 <div class="social-button facebook">
