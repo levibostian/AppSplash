e61ba3d8ee858b8ee0317d8c991821adc2e43f5c
Levi Bostian <levi.bostian@gmail.com>
2015-03-28 19:49:54 -0500

    Redo layout of columns and rows to fix mobile issues.

--- index.php
+++ index.php
@@ -1,67 +1,68 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Document</title>
+    <head>
+        <meta charset="UTF-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Document</title>
 
-    <!-- Bootstrap -->
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
-    <link href="http://fonts.googleapis.com/css?family=Roboto:300" rel="stylesheet" type="text/css">
-    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+        <!-- Bootstrap -->
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+        <link href="http://fonts.googleapis.com/css?family=Roboto:300" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 
-    <link rel="stylesheet" href="style.css">
-  </head>
-  <body>
-    <div class="container-fluid app-header">
-      <div class="container">
-        <div class="row">
-          <div>
-            <img alt="app logo" src="img/app-logo.png" height="75px" width="75px"/>
-            <h1 class="inline-text" style="padding-left: 10px;">App Name</h1>
-          </div>
-        </div>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <div class="container-fluid app-header">
 
-        <div class="row"> 
-          <div class="col-md-offset-1 col-md-6 center-table">
-            <div class="center-table-cell">
-              <h2 class="description-header">App for people to do stuff.</h2>
-              <p class="description">Dr. Jimmy Brungus forgot to do rockets. Hippie Joel tastes like cow bathroom. Not for horses. No lonely times just dreams you turkey. Stop, drop and roll bones made from stuff your muscles don't like. Check the expiration date used to love pruppets. </p>
-            </div>
-          </div>
-          <div class="col-md-4 app-screenshot-container">
-            <img alt="app screenshot" src="img/app-screenshot.png" height="700px"/>
-          </div>
-        </div>
-      </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-7">
+                        <div class="row">
+                            <img alt="app logo" src="img/app-logo.png" height="75px" width="75px"/>
+                            <h1 class="inline-text" style="padding-left: 10px;">App Name</h1>
+                        </div><!-- row -->
 
-      <div class="row connect-row">
-        <div class="container">
-          <div class="col-md-offset-6 col-md-6">
-            <div class="center-block">
-              <a href="#" class="white">
-                <div class="social-button facebook">
-                  <i class="fa fa-facebook fa-4x icon"></i>
-                </div>
-              </a>
-              
-              <a href="#" class="white">
-                <div class="social-button twitter" href="#">
-                  <i class="fa fa-twitter fa-4x icon"></i>
-                </div>
-              </a>
+                        <div class="row">
+                            <div class="col-md-offset-1 col-md-11">
+                                <h2 class="description-header">App for people to do stuff.</h2>
+                                <p class="description">Dr. Jimmy Brungus forgot to do rockets. Hippie Joel tastes like cow bathroom. Not for horses. No lonely times just dreams you turkey. Stop, drop and roll bones made from stuff your muscles don't like. Check the expiration date used to love pruppets. </p>
+                            </div>
+                        </div><!-- row -->
+                    </div><!-- col-md-7 -->
+                    <div class="col-md-5 app-screenshot-container">
+                        <img alt="app screenshot" src="img/app-screenshot.png" height="625px"/>
+                    </div>
+                </div><!-- row -->
+            </div><!-- container -->
 
-              <a href="#" class="white">
-                <div class="social-button instagram" href="#">
-                  <i class="fa fa-instagram fa-4x icon"></i>
-                </div>
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </body>
+            <div class="row connect-row">
+                <div class="container">
+                    <div class="col-md-offset-6 col-md-6">
+                        <div class="center-block">
+                            <a href="#" class="white">
+                                <div class="social-button facebook">
+                                    <i class="fa fa-facebook fa-4x icon"></i>
+                                </div>
+                            </a>
+                            
+                            <a href="#" class="white">
+                                <div class="social-button twitter" href="#">
+                                    <i class="fa fa-twitter fa-4x icon"></i>
+                                </div>
+                            </a>
+
+                            <a href="#" class="white">
+                                <div class="social-button instagram" href="#">
+                                    <i class="fa fa-instagram fa-4x icon"></i>
+                                </div>
+                            </a>
+                        </div><!-- center-block -->
+                    </div><!-- col-md-offset-6 col-md-6 -->
+                </div><!-- container -->
+            </div><!-- row connect-row -->
+        </div><!-- container-fluid -->
+    </body>
 </html>
