c025524ebf1f977a0b163b2fc7b1387dd787daca
Levi Bostian <levi.bostian@gmail.com>
2015-03-29 19:27:21 -0500

    Fix layout with correct containers and rows bootstrap.

--- index.php
+++ index.php
@@ -16,27 +16,29 @@
         <link rel="stylesheet" href="style.css">
     </head>
     <body>
-        <div class="container-fluid app-header">
+        <div class="container-fluid">
+            <div class="row app-header">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-7">
+                            <div class="row">
+                                <img alt="app logo" src="img/app-logo.png" height="75px" width="75px"/>
+                                <h1 class="inline-text" style="padding-left: 10px;">App Name</h1>
+                            </div><!-- row -->
 
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-7">
-                        <div class="row">
-                            <img alt="app logo" src="img/app-logo.png" height="75px" width="75px"/>
-                            <h1 class="inline-text" style="padding-left: 10px;">App Name</h1>
-                        </div><!-- row -->
-
-                        <div class="row">
-                            <div class="col-md-offset-1 col-md-11">
-                                <h2 class="description-header">App for people to do stuff.</h2>
-                                <p class="description">Dr. Jimmy Brungus forgot to do rockets. Hippie Joel tastes like cow bathroom. Not for horses. No lonely times just dreams you turkey. Stop, drop and roll bones made from stuff your muscles don't like. Check the expiration date used to love pruppets. </p>
-                            </div>
-                        </div><!-- row -->
-                    </div><!-- col-md-7 -->
-                    <div class="col-md-5 app-screenshot-container">
-                        <img alt="app screenshot" src="img/app-screenshot.png" height="625px"/>
-                    </div>
-                </div><!-- row -->
+                            <div class="row">
+                                <div class="col-md-offset-1 col-md-11">
+                                    <h2 class="description-header">App for people to do stuff.</h2>
+                                    <p class="description">Dr. Jimmy Brungus forgot to do rockets. Hippie Joel tastes like cow bathroom. Not for horses. No lonely times just dreams you turkey. Stop, drop and roll bones made from stuff your muscles don't like. Check the expiration date used to love pruppets. </p>
+                                </div>
+                            </div><!-- row -->
+                        </div><!-- col-md-7 -->
+                        <div class="col-md-5 app-screenshot-container">
+                            <img alt="app screenshot" src="img/app-screenshot.png" height="625px"/>
+                        </div>
+                    </div><!-- row -->
+                </div>
+            </div>
 
 
             <div class="row connect-row">
@@ -57,11 +59,22 @@
 		                            <div class="response" id="mce-success-response" style="display:none"></div>
 	                                </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
                                         <div style="position: absolute; left: -5000px;"><input type="text" name="b_7f489ab74730d111936a8515e_e7c869cc60" tabindex="-1" value=""></div>
-                                        <div class="clear"><input type="submit" value="Notify" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+                                        <div class="clear">
+                                            <input type="submit" value="Notify" name="subscribe" id="mc-embedded-subscribe" class="button">
+                                        </div>
                                     </div>
                                 </form>
                             </div>
-                            <script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
+                            <script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script>
+                            <script type='text/javascript'>
+                             (function($) {
+                                 window.fnames = new Array();
+                                 window.ftypes = new Array();
+                                 fnames[0]='EMAIL';
+                                 ftypes[0]='email';
+                             }(jQuery));
+                             var $mcj = jQuery.noConflict(true);
+                            </script>
                             <!--End mc_embed_signup-->
                             
                         </div><!-- col-md-6 -->
@@ -75,13 +88,13 @@
                                 </a>
                                 
                                 <a href="#" class="white">
-                                    <div class="social-button twitter" href="#">
+                                    <div class="social-button twitter">
                                         <i class="fa fa-twitter fa-4x icon"></i>
                                     </div>
                                 </a>
 
                                 <a href="#" class="white">
-                                    <div class="social-button instagram" href="#">
+                                    <div class="social-button instagram">
                                         <i class="fa fa-instagram fa-4x icon"></i>
                                     </div>
                                 </a>
@@ -90,7 +103,6 @@
                     </div><!-- row -->
                 </div><!-- container -->
             </div><!-- row connect-row -->
-                        </div><!-- container -->
         </div><!-- container-fluid -->
     </body>
 </html>
