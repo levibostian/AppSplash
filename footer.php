431aec06e47d31a6862baaa6bd0c64a266401011
Levi Bostian <levi.bostian@gmail.com>
2015-03-31 13:06:09 -0500

    Create index.php and footer.php for Wordpress theme. Old index.php file is now static_index.php

---
+++ footer.php
@@ -0,0 +1,38 @@
+<div class="row footer-row">
+                <div class="container">
+                    <div class="row"><!-- try not to edit height here. makes social buttons align along top -->
+                        <div class="col-md-12">
+                            <div class="center-hor-vert" style="z-index: 150;">
+                                <a href="#" class="social-button-a-link">
+                                    <div class="social-button facebook">
+                                        <i class="fa fa-facebook fa-4x icon"></i>
+                                    </div>
+                                </a>
+                                
+                                <a href="#" class="social-button-a-link">
+                                    <div class="social-button twitter">
+                                        <i class="fa fa-twitter fa-4x icon"></i>
+                                    </div>
+                                </a>
+
+                                <a href="#" class="social-button-a-link">
+                                    <div class="social-button instagram">
+                                        <i class="fa fa-instagram fa-4x icon"></i>
+                                    </div>
+                                </a>
+                            </div><!-- center-hor-vert -->
+                        </div><!-- col-md-offset-6 col-md-6 -->
+                    </div><!-- row -->
+
+                    <div class="row">
+                        <div class="col-md-12 footer-row-height">
+                            <!-- footer content here -->
+                        </div>
+                    </div>
+                </div><!-- container -->
+            </div><!-- row footer-row -->
+        </div><!-- container-fluid -->
+
+        <?php wp_footer(); ?>
+    </body>
+</html>
---
+++ static_index.php
@@ -0,0 +1,127 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Document</title>
+
+        <!-- Bootstrap -->
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+        <link href="http://fonts.googleapis.com/css?family=Roboto:300" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+        <link href="//cdn-images.mailchimp.com/embedcode/classic-081711.css" rel="stylesheet" type="text/css">
+
+        <link rel="stylesheet" href="style.css">
+        <script src="dist/app-splash.min.js"></script>
+        <script src="dist/bootstrap.min.js"></script>
+    </head>
+    <body>
+        <div class="container-fluid">
+            <div class="row app-header">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-7" style="height: 500px;">
+                            <div class="row">
+                                <img class="app-logo" alt="app logo" src="img/app-logo.png" height="75px" width="75px"/>
+                                <h1 class="inline-text app-name" style="padding-left: 10px;">App Name</h1>
+                            </div><!-- row -->
+
+                            <div class="row" style="height: 82%;">
+                                <div class="col-md-offset-1 col-md-11 center-div">
+                                    <h2 class="description-header">App for people to do stuff.</h2>
+                                    <p class="description">Dr. Jimmy Brungus forgot to do rockets. Hippie Joel tastes like cow bathroom. Not for horses. No lonely times just dreams you turkey. Stop, drop and roll bones made from stuff your muscles don't like. Check the expiration date used to love pruppets. </p>
+                                </div>
+                            </div><!-- row -->
+                        </div><!-- col-md-7 -->
+                        <div class="col-md-5 app-screenshot-container">
+                          <img class="image-center app-screenshot"  src="img/app-screenshot-frame.png"/>
+                        </div>
+                    </div><!-- row -->
+                </div><!-- container -->
+            </div><!-- row app-header -->
+
+
+            <div class="row connect-row">
+                <div class="container" style="height: auto;">
+                    <div class="row" style="height: auto;">
+                        <div class="col-md-6 connect-row-height">
+                            <div class="center-div">
+                                <h2>Coming soon. <br/>
+                                    Get notified when app is released.
+                                </h2>
+                            </div>
+                        </div>
+                        <div class="col-md-6 connect-row-height">
+
+                            <!-- Begin MailChimp Signup Form -->
+                            <div id="mc_embed_signup" class="center-div mailchimp-container">
+                                <form action="//levibostian.us10.list-manage.com/subscribe/post?u=7f489ab74730d111936a8515e&amp;id=e7c869cc60" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+                                    <div id="mc_embed_signup_scroll">
+	                                
+                                        <div class="mc-field-group mc_field_headers">
+	                                    <input type="email" placeholder="Email Address" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+                                        </div>
+	                                <div id="mce-responses" class="clear">
+		                            <div class="response" id="mce-error-response" style="display:none"></div>
+		                            <div class="response" id="mce-success-response" style="display:none"></div>
+	                                </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                                        <div style="position: absolute; left: -5000px;"><input type="text" name="b_7f489ab74730d111936a8515e_e7c869cc60" tabindex="-1" value=""></div>
+                                        <div class="clear">
+                                            <input type="submit" value="Notify" name="subscribe" id="mc-embedded-subscribe" class="button">
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                            <script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script>
+                            <script type='text/javascript'>
+                             (function($) {
+                                 window.fnames = new Array();
+                                 window.ftypes = new Array();
+                                 fnames[0]='EMAIL';
+                                 ftypes[0]='email';
+                             }(jQuery));
+                             var $mcj = jQuery.noConflict(true);
+                            </script>
+                            <!--End mc_embed_signup-->
+                            
+                        </div><!-- col-md-->                              
+                    </div><!-- row -->
+                </div><!-- container -->
+            </div><!-- row connect-row -->
+
+            <div class="row footer-row">
+                <div class="container">
+                    <div class="row"><!-- try not to edit height here. makes social buttons align along top -->
+                        <div class="col-md-12">
+                            <div class="center-hor-vert" style="z-index: 150;">
+                                <a href="#" class="social-button-a-link">
+                                    <div class="social-button facebook">
+                                        <i class="fa fa-facebook fa-4x icon"></i>
+                                    </div>
+                                </a>
+                                
+                                <a href="#" class="social-button-a-link">
+                                    <div class="social-button twitter">
+                                        <i class="fa fa-twitter fa-4x icon"></i>
+                                    </div>
+                                </a>
+
+                                <a href="#" class="social-button-a-link">
+                                    <div class="social-button instagram">
+                                        <i class="fa fa-instagram fa-4x icon"></i>
+                                    </div>
+                                </a>
+                            </div><!-- center-hor-vert -->
+                        </div><!-- col-md-offset-6 col-md-6 -->
+                    </div><!-- row -->
+
+                    <div class="row">
+                        <div class="col-md-12 footer-row-height">
+                            <!-- footer content here -->
+                        </div>
+                    </div>
+                </div><!-- container -->
+            </div><!-- row footer-row -->
+        </div><!-- container-fluid -->
+    </body>
+</html>
