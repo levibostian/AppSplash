2315d2b80b3ff691477a4cfa225d303c25266201
Levi Bostian <levi.bostian@gmail.com>
2015-04-07 00:09:42 -0500

    Release 1.0.3 to fix header to make mobile view work better.

--- header.php
+++ header.php
@@ -2,6 +2,7 @@
 <html lang="en">
     <head>
         <meta charset="<?php bloginfo('charset'); ?>">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title><?php bloginfo('name'); ?> | <?php is_front_page() ? bloginfo('description') : wp_title(''); ?></title>
 
